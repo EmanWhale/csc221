64c06b97c33dd411fe8a8521e22b51aab8a136f1
Emmanuel Aregay <eaboss1000@gmail.com>
2023-07-23 22:43:38 -0400

    Added while loop

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -1,26 +1,18 @@
 from random import randint
 
-guesses = 0
-guesses = guesses + 0
+while True:
+    number = randint(1, 1000)
+    question = "I have chosen a number between 1 and 1000. Guess my number:"
+    answer = int(input(question))
+    guesses = 0
+    if answer < number:
+            print ("That is too low. Try again!")
+            guesses += 1
 
-number = randint(1, 1000)
+    if answer > number:
+            print ("That is too high. Try again!")
+            guesses += 1
 
-question = "I have chosen a number between 1 and 1000. Guess my number:"
-
-answer = int(input(question))
-
-if answer < number:
-        print ("That is too low. Try again!")
-        guesses += 1
-
-if answer > number:
-        print ("That is too high. Try again!")
-        guesses += 1
-
-if answer == number:
+    if answer == number:
         print ("CONGRATS! You correctly guessed my number!")
-else: 
-       answer = int(input(question))
-
-
-    
+        break
