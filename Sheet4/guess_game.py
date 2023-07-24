34d87259fcbcaac79917237cdc618156e4374578
Emmanuel Aregay <eaboss1000@gmail.com>
2023-07-23 23:16:54 -0400

    Fixed infinite loop of prints

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -6,9 +6,7 @@ number = randint(1, 1000)
 question = "I have chosen a number between 1 and 1000. Guess my number:"
 answer = int(input(question))
 
-while answer != number:
-    question = "I have chosen a number between 1 and 1000. Guess my number:"
-    answer = int(input(question))
+while True:
     if answer < number:
             print ("That is too low. Try again!")
             guesses += 1
@@ -17,5 +15,8 @@ while answer != number:
             print ("That is too high. Try again!")
             guesses += 1
 
-print ("CONGRATS! You correctly guessed my number! It took you " + str(guesses) + " guesses.")
+    if answer == number:
+            print ("CONGRATS! You correctly guessed my number! It took you " + str(guesses) + " guesses.")
+            
+
 
