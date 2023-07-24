8c98d3033566d842906321b3b47cb0c072d992a8
Emmanuel Aregay <eaboss1000@gmail.com>
2023-07-23 23:37:39 -0400

    Correct amount of guesses

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -6,7 +6,6 @@ number = randint(1, 1000)
 while True:
     question = "I have chosen a number between 1 and 1000. Guess my number:"
     answer = int(input(question))
-    guesses = 0
     if answer < number:
             print ("That is too low. Try again!")
             guesses += 1
