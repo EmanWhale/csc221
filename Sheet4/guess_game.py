0bf4e96437b84feca17b23b9c1db4b5fedf0f68e
Emmanuel Aregay <eaboss1000@gmail.com>
2023-07-23 23:05:43 -0400

    Fixed amount of guesses

--- Sheet4/guess_game.py
+++ Sheet4/guess_game.py
@@ -1,11 +1,14 @@
 from random import randint
 
+guesses = 0
 number = randint(1, 1000)
 
-while True:
+question = "I have chosen a number between 1 and 1000. Guess my number:"
+answer = int(input(question))
+
+while answer != number:
     question = "I have chosen a number between 1 and 1000. Guess my number:"
     answer = int(input(question))
-    guesses = 0
     if answer < number:
             print ("That is too low. Try again!")
             guesses += 1
@@ -14,6 +17,5 @@ while True:
             print ("That is too high. Try again!")
             guesses += 1
 
-    if answer == number:
-        print ("CONGRATS! You correctly guessed my number!")
-        break
+print ("CONGRATS! You correctly guessed my number! It took you " + str(guesses) + " guesses.")
+
